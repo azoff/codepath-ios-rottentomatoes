48b1f1776612a640e80aeabfdcf03774e045b631
Jonathan Azoff <jon@azoffdesign.com>
2014-03-13 19:50:30 -0700

    Initial Commit

---
+++ rottentomatos/az_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  az_AppDelegate.h
+//  rottentomatos
+//
+//  Created by Jonathan Azoff on 3/13/14.
+//  Copyright (c) 2014 Jonathan Azoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface az_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rottentomatos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rottentomatos
+//
+//  Created by Jonathan Azoff on 3/13/14.
+//  Copyright (c) 2014 Jonathan Azoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "az_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([az_AppDelegate class]));
+    }
+}
